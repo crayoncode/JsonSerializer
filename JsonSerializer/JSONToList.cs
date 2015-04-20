5cb5db74ae7f62206c7632110c54086a9b814a59
crayoncode <crayoncode@163.com>
2015-04-20 15:47:53 +0800

    Version1.0 json序列化与反序列化：

---
+++ JsonSerializer/JSONToList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace JsonSerializer
+{
+    public static class JSONToList
+    {
+        public static List<T> JSONStringToList<T>(string JsonStr)
+        {
+            JavaScriptSerializer Serializer = new JavaScriptSerializer();
+            List<T> objs = Serializer.Deserialize<List<T>>(JsonStr);
+            return objs;
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            T obj = Activator.CreateInstance<T>();
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+                return (T)serializer.ReadObject(ms);
+            }
+        }  
+    }
+}
---
+++ JsonSerializer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+
+namespace JsonSerializer
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //字符串发序列化为对象
+            string jsonStr = "[{Name:'苹果',Price:5.5},{Name:'橘子',Price:2.5},{Name:'柿子',Price:16}]";
+            List<Product> products = new List<Product>();
+            products = JSONToList.JSONStringToList<Product>(jsonStr);
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + ":" + item.Price + "<br />");
+                
+            }
+            //对象序列化成json字符串
+            Console.WriteLine(GetJsonString());
+            Console.ReadKey();
+        }
+
+        public static string GetJsonString()         
+        {
+              List<Product> products = new List<Product>() { 
+              new Product(){Name="苹果",Price=3.9},
+              new Product(){Name="橘子",Price=2.5},  
+              new Product(){Name="干柿子",Price=16.00}
+            };
+              ProductList productlist = new ProductList();
+              productlist.GetProducts = products;
+              return new JavaScriptSerializer().Serialize(productlist);
+        }
+        public class ProductList
+        {
+            public List<Product> GetProducts { get; set; }
+        } 
+       
+    }
+}
